32e66e39bf448829eb39b558a398904000a3281a
Yahia-MhZ <161332542+Yahia-MhZ@users.noreply.github.com>
2024-08-31 12:45:08 +0000

    dev ratt

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,60 @@
-use nix::libc::{sigaction, siginfo_t, SigAction, SigHandler, SigSet, SIGSEGV};
+use nix::libc::siginfo_t;
 use std::error::Error;
-use std::fs::File;
 use std::os::raw::{c_int, c_void};
-use std::os::unix::io::AsRawFd;
-use std::slice;
 
-mod runner;
+mod runner;  
 
 extern "C" fn sigsegv_handler(_signal: c_int, siginfo: *mut siginfo_t, _extra: *mut c_void) {
     let address = unsafe { (*siginfo).si_addr() } as usize;
 
-    eprintln!("Segmentation fault at address: 0x{:x}", address);
 }
 
 fn exec(filename: &str) -> Result<(), Box<dyn Error>> {
-    let file = File::open(filename)?;
-    let mmap = unsafe {
-        mmap::map(
-            file.as_raw_fd(),
-            mmap::AccessType::Read,
-            0,
-            mmap::map_len(file.metadata()?),
-        )?
-    };
+    // Read and parse the ELF file
+    let elf_file = runner::parse_elf_file(filename)?;
 
-    let ehdr = unsafe { &*(mmap.ptr as *const Elf32_Ehdr) };
-    let phdr = unsafe {
-        slice::from_raw_parts(
-            mmap.ptr.offset(ehdr.e_phoff as isize) as *const Elf32_Phdr,
-            ehdr.e_phnum as usize,
-        )
-    };
-
-    for (i, ph) in phdr.iter().enumerate() {
+    
+    for (i, segment) in elf_file.segments.iter().enumerate() {
         eprintln!(
             "{}\t0x{:x}\t{}\t0x{:x}\t{}\t{}",
             i,
-            ph.p_vaddr,
-            ph.p_filesz,
-            ph.p_offset,
-            ph.p_memsz,
-            ph.p_flags
+            segment.address,
+            segment.size,
+            segment.offset,
+            segment.length,
+            segment.flags
         );
     }
 
-    let base_address = phdr
-        .iter()
-        .map(|ph| ph.p_vaddr)
-        .min()
-        .ok_or("Failed to determine Base address")?;
-
-    let entry_point = ehdr.e_entry;
+    
+    let base_address = elf_file.base_address;
+    let entry_point = elf_file.entry_point;
 
     eprintln!("Base address 0x{:x}", base_address);
     eprintln!("Entry point 0x{:x}", entry_point);
 
+  
     unsafe {
-        let sigaction = SigAction::new(
-            SigHandler::SigAction(sigsegv_handler),
-            SigSet::empty(),
-            0,
-        );
-        sigaction::sigaction(SIGSEGV, &sigaction)?;
+        let mut sa = std::mem::zeroed();
+        sa.sa_sigaction = sigsegv_handler as usize;
+        sa.sa_flags = nix::libc::SA_SIGINFO;
+        nix::libc::sigaction(nix::libc::SIGSEGV, &sa, std::ptr::null_mut());
     }
 
-    runner::exec_run(base_address as usize, entry_point as usize);
+
+    runner::exec_run(base_address, entry_point);
 
     Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+   
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: {} <ELF file>", args[0]);
+        eprintln!("Usage: {} <filename>", args[0]);
         std::process::exit(1);
     }
+
+    // Execute elf file
     exec(&args[1])
 }
--- src/runner/mod.rs
+++ src/runner/mod.rs
@@ -1,8 +1,8 @@
 use nix::libc::{Elf32_Ehdr, Elf32_Phdr};
 use std::arch::asm;
+use std::fs::File;
+use std::io::Read;
 use std::env;
-use std::ffi::CStr;
-use std::ptr;
 
 #[repr(C)]
 #[derive(Copy, Clone)]
@@ -27,33 +27,88 @@ extern "C" {
     static environ: *mut *mut u8;
 }
 
+pub struct Segment {
+    pub address: usize,
+    pub size: usize,
+    pub offset: usize,
+    pub length: usize,
+    pub flags: String,
+}
+
+pub struct ElfFile {
+    pub segments: Vec<Segment>,
+    pub base_address: usize,
+    pub entry_point: usize,
+}
+
+pub fn parse_elf_file(filename: &str) -> Result<ElfFile, Box<dyn std::error::Error>> {
+    let mut file = File::open(filename)?;
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer)?;
+
+    let ehdr = unsafe { &*(buffer.as_ptr() as *const Elf32_Ehdr) };
+    let mut segments = Vec::new();
+
+    for i in 0..ehdr.e_phnum {
+        let phdr = unsafe {
+            &*(buffer.as_ptr().offset(ehdr.e_phoff as isize + (i * std::mem::size_of::<Elf32_Phdr>()) as isize) as *const Elf32_Phdr)
+        };
+
+        let flags = format!(
+            "{}{}{}",
+            if phdr.p_flags & 1 != 0 { 'x' } else { '-' },
+            if phdr.p_flags & 2 != 0 { 'w' } else { '-' },
+            if phdr.p_flags & 4 != 0 { 'r' } else { '-' }
+        );
+
+        segments.push(Segment {
+            address: phdr.p_vaddr as usize,
+            size: phdr.p_memsz as usize,
+            offset: phdr.p_offset as usize,
+            length: phdr.p_filesz as usize,
+            flags,
+        });
+    }
+
+    Ok(ElfFile {
+        segments,
+        base_address: segments[0].address,
+        entry_point: ehdr.e_entry as usize,
+    })
+}
+
 pub fn exec_run(base_address: usize, entry_point: usize) {
+    // Access the ELF header
     let ehdr = unsafe { &*(base_address as *const u8 as *const Elf32_Ehdr) };
-    let phdr =
-        unsafe { &*((base_address + (*ehdr).e_phoff as usize) as *const u8 as *const Elf32_Phdr) };
+    
+    // Access the program header
+    let phdr = unsafe { 
+        &*((base_address + (*ehdr).e_phoff as usize) as *const u8 as *const Elf32_Phdr) 
+    };
 
     let mut auxv;
 
+    // Access the environment variables and auxiliary vectors
     let env_address = unsafe {
         let mut env = environ;
 
-        // skip environment variables
+        // Skip environment variables
         while !(*env).is_null() {
             env = env.offset(1);
         }
-
         env = env.offset(1);
-
         auxv = &mut *(env as *mut u8 as *mut Elf32AuxV);
 
+      
         let argv = environ.offset(-(env::args().len() as isize + 2));
 
         *argv.offset(2) = *argv.offset(1);
-        *argv.offset(1) = (env::args().len() - 1) as *mut u8;
+        *argv.offset(1) = (env::args().len()-1) as *mut u8;
 
         argv.offset(1)
     };
 
+   
     while auxv.a_type != AT_NULL {
         match auxv.a_type {
             AT_PHDR => auxv.a_un.a_val = phdr as *const Elf32_Phdr as u32,
